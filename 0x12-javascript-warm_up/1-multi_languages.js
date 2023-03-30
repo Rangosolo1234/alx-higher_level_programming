fa6a44ad18396643e310f18da14afa31a4c5dff5
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-03-30 14:11:37 +0000

    Task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// It prints three separate lines of sentences
-
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
