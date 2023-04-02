b3c666e32ff02b07b5a6602f7dfc3e25a56d1cb5
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-02 08:07:33 -0700

    Checks if object is instance of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+===================================
+============================
+===================================
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Method returns true if object is instance of class"""
+
+    return isinstance(obj, a_class)
