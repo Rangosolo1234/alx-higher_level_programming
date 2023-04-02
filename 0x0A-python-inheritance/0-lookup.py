a68d3c92bac7fc750c248bd610fce00ba781268a
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-02 07:18:16 -0700

    Methods and attributes list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """Function for return the attributes for an object"""    
+    """Returns the list of mettods and attributes"""    
 
-    attributes = []
-    methods = []
-    for attr in dir(obj):
-        if callable(getattr(obj, attr)):
-            methods.append(attr)
-        else:
-            attributes.append(attr)
-    return attributes + methods
+    return dir(obj)
