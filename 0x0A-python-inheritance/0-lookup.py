8767204d65dc7da83860d38f2d1a6b2b49f37d7e
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-02 07:29:59 -0700

    Task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Function lookup that returns the list of methods
+used in a block of code
+=============
+"""
+
 def lookup(obj):
-    """Returns the list of mettods and attributes"""
+"""Returns the list of mettods and attributes"""
 
     return dir(obj)
