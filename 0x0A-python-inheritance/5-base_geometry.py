5446d52953d58bb0a0c9d117f35684711d11efcc
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-02 08:17:55 -0700

    Empty class and pass

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+===================================
+-----------------------
+===================================
+"""
+
+
+class BaseGeometry:
+    """empty class and pass to avoid error message"""
+    pass
