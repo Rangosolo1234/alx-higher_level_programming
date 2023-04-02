a3b1bd1e68a5de8572330a5ff376eefe62903580
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-02 08:00:12 -0700

    Task2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===========================
+No module imported
+===========================
+"""
+
+
+class MyList(list):
+    """Class with a list argument"""
+    pass
+
+    def print_sorted(self):
+        """Method to return sorted list"""
+
+        print(sorted(list(self)))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+================================
+============================
+================================
+"""
+
+
+def is_same_class(obj, a_class):
+    """ returns True if the object is exactly an instance of a class"""
+
+    return type(obj) is a_class
