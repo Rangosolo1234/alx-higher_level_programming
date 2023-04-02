a680d8d6dffc9d003dca7cc68e6671ef789d5c12
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-02 07:04:56 -0700

    List of Attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def lookup(obj):
-    return(dir(obj))
+    attributes = []
+    methods = []
+    for attr in dir(obj):
+        if callable(getattr(obj, attr)):
+            methods.append(attr)
+        else:
+            attributes.append(attr)
+    return attributes + methods
