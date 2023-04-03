3760e0e4f15424c990b936c3000709fd2478ec5d
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-03 06:41:28 -0700

    Readfile

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """function to read text file and print output"""
+"""
+function to read text file
+"""
 
-    with open(filename) as f:
-        text = f.read()
-        print(text, end="")
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout
+    Returns none
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
