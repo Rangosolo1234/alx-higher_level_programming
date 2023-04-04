2dcc890beb40cf70b64a4cd43f736798e6211e7f
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-04 16:50:14 -0700

    Correct100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that lists list 10 commits (from the most recent
+to oldest) of the repository “rails” by the user “rails”."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
+                                                              sys.argv[1])
+    r = requests.get(url)
+    try:
+        for i in range(0, 10):
+            a = r.json()[i].get('sha')
+            b = r.json()[i].get('commit').get('author').get('name')
+            print("{}: {}".format(a, b))
+    except IndexError:
+        pass
