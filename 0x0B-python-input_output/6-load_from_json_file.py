322cc2a167cd49faef058fc6a806eee7ee585158
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-04 03:53:03 -0700

    Python object from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""-----------------------------"""
+import json
+
+
+def load_from_json_file(filename):
+    """python object from json file"""
+    with open(filename) as f:
+        return json.load(f)
