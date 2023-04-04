ab06b805760e31b4fa98145c339d7290d55aec91
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-04 03:40:46 -0700

    To json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' function that returns the JSON representation of an object string
+'''
+
+import json
+
+
+def to_json_string(my_obj):
+    ''' module to_json_string
+    '''
+    return json.dumps(my_obj)
