4e44cd161bb2b0d8d6b978c0e371a9402eb661be
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-04 16:27:34 -0700

    Intranet status request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status."""
+"""Python script that fetches https://intranet.hbtn.io/status.
+----------------------------------------------------------
+=========================================="""
 
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type:', type(r.text))
-    print('\t- content:', r.text)
+    reuq = requests.get("https://intranet.hbtn.io/status").text
+    print("Body response:")
+    print("\t- type: {}".format(type(reuq)))
+    print("\t- content: {}".format(reuq))
