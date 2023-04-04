56ac6d3d9e16a4b07d0c39433b2ece0d122d53ba
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-04 15:51:47 -0700

    Display value of request id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python script that takes in a URL, sends a request to the URL and displays
+# the value of the X-Request-Id variable
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
