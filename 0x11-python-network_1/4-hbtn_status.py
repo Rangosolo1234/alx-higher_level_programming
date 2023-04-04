470d988111071fb50a6ffa6a34434e58afab8172
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-04 16:31:01 -0700

    Task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status.
-----------------------------------------------------------
-=========================================="""
+"""Python script that fetches https://intranet.hbtn.io/status"""
 
 import requests
 
