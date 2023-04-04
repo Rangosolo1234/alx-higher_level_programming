17261b23cd3cd8ca9968ac2d6e48358e4618f654
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-04 04:29:53 -0700

    Technical interview prep

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Represents Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
