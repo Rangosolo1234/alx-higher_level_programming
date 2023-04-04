9c685dffee7b11483b36ae7e124a7bc076b4e76f
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-04 16:16:08 -0700

    TaskUpdate Error message

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,15 @@
 """Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)."""
 
-from urllib import request, error
-from sys import argv
+import urllib.request
+import urllib.error
+import sys
 
 if __name__ == "__main__":
-        try:
-            with request.urlopen(argv[1]) as page:
-                print(page.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    req = urllib.request.Request(sys.argv[1])
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf 8'))
+    except urllib.error.HTTPError as e:
+        print('Error code:', e.code)
