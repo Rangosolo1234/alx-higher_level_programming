d1e6bd4d7df2cba4c0e6181c838e7d8294e529ca
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-24 06:23:52 -0700

    Creating a table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Creating a table in SQL for MySQL server
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
