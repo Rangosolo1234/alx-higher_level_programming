0d07927520ba86e93d37b437f2e30ff3099169ec
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-25 06:08:21 -0700

    Grouping data from a table

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- Groups items of a table with same values of score and prints them in decsending order
+SELECT `score`, COUNT(score) AS number FROM `second_table` GROUP BY `score` ORDER BY `score`;
