68fad1d4378bc612168ffb214b99610d8016e57c
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-25 05:39:07 -0700

    Selecting data that meet a specified condition and ordering them

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Lists all records of a table whose values are greater than 10
-SELECT score, name FROM score_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
