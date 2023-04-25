5de2c9d6541681d4c104ff2c71ee1ccdc159656a
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-25 05:28:46 -0700

    Order items of a table in a database

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Selects name and score records of second_table
 -- Ranks the names using the score value
-SELECT `score`, `name` FROM `score_table` ORDER BY `score` DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
