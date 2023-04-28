57f9e6b8ebc5e6c28c064f2e94e4e9da2146f4d8
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-28 06:01:33 -0700

    Working with foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Creates databse hbtn_0d_usa
+-- Creates table named cities
+-- cities contains attributes as follows
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can’t be null
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       state_id INT NOT NULL,
+       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+       name VARCHAR(256) NOT NULL);
