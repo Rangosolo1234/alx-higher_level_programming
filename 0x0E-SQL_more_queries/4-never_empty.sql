566d1a60b48ab8d86bdfe39d4bb821e0cb438fde
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-28 07:18:17 -0700

    Id cannot be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -2,4 +2,8 @@
 -- Attributes id INT with the default value 1, name VARCHAR(256)
 -- If table exists should not fail
 -- Cannot contail null value instead 1 by default
-REATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+    `id` INT DEFAULT 1,
+    `name` VARCHAR(256)
+);
