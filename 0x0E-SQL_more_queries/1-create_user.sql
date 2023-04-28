882c94bc03d108b7e0d9480d17498a774a1a4a02
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-28 05:15:11 -0700

    Grant full privilege to user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,11 +1,6 @@
 -- Create user named user_0d_1 if not in existance
 -- Grant all privileges to the user
 -- Password set to user_0d_1_pwd
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
