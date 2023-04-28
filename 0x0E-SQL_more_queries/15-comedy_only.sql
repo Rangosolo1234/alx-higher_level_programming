d40e36b6c6dfeaa38724320fc45bb4d0bae6dbe9
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-28 06:46:01 -0700

    More queries this one displays Comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy
+-- (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
