950b98756e6d1dfb0075c0cfba8b30d9c1e1959c
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-04-28 05:32:05 -0700

    Granting only one privilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
 -- Create database and in database create user named user_0d_2
 -- Should not fail if database or user already exists
--- User is granted only SELECT privilege
-CREATE DATABSE IF NOT EXISTS `hbtn_0d_2`;
+-- User is granted only SELECT privile.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
